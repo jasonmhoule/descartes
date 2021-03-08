d014b8c1e8167eddf9a3c18ec45c4aa8818f1e7d
Jason Houle <jasonmhoule@gmail.com>
2021-03-08 14:17:10 -0600

    Update start.sh

--- config/start.sh
+++ config/start.sh
@@ -2,8 +2,8 @@
 
 echo "SELENIUM_IP=$SELENIUM_IP" >> usr/local/lib/R/etc/Renviron
 
-mkdir -p /home/$USER/.ssh
-gsutil cp -r gs://jmh_config/jmh_config/.ssh /home/$USER/.ssh
+mkdir -p /home/$USER
+gsutil cp -r gs://jmh_config/jmh_config/.ssh /home/$USER
 gsutil cp -r gs://jmh_config/jmh_config/.gitconfig /home/$USER
 
 /init
