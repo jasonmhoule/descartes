5888c1661e6fb807a6363b36a3ee01b51a6eb7bd
Jason Houle <jasonmhoule@gmail.com>
2021-05-05 20:26:07 -0500

    Update start.sh

--- config/docker/start.sh
+++ config/docker/start.sh
@@ -5,6 +5,6 @@ echo "SELENIUM_IP=$SELENIUM_IP" >> usr/local/lib/R/etc/Renviron
 mkdir -p /home/$USER
 gsutil cp -r gs://jmh_config/jmh_config/.ssh /home/$USER
 gsutil cp -r gs://jmh_config/jmh_config/.gitconfig /home/$USER
-chmod 400 /home/$USER/.ssh/id_rsa
+chmod --verbose 400 /home/$USER/.ssh/id_rsa
 
 /init
